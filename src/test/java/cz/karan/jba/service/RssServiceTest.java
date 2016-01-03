b3687cd1049389059801cc7812ebb9df98e776be
Tank <karantankshalifarji@gmail.com>
2016-01-03 19:37:04 +0530

    chnaged javavids.xml file as it was not the one i wanted

---
+++ src/test/java/cz/karan/jba/service/RssServiceTest.java
@@ -0,0 +1,28 @@
+package cz.karan.jba.service;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cz.karan.jba.entity.Item;
+import cz.karan.jba.exception.RssException;
+
+public class RssServiceTest {
+
+	private RssService rssService;
+	@Before
+	public void setUp() throws Exception {
+		rssService = new RssService();
+	}
+
+	@Test
+	public void testGetItemsFile() throws RssException{
+		List<Item> items = rssService.getItems(new File("test-rss/javavids.xml"));
+		assertEquals(10, items.size());
+	}
+
+}
