b47448cbe8a306530cdb7696436d58a7f238b659
Tank <karantankshalifarji@gmail.com>
2016-01-03 20:56:06 +0530

    Added Scheduling

--- src/main/java/cz/karan/jba/service/BlogService.java
+++ src/main/java/cz/karan/jba/service/BlogService.java
@@ -3,6 +3,7 @@ package cz.karan.jba.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.access.method.P;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
@@ -69,5 +70,13 @@ public class BlogService {
 			e.printStackTrace();
 		}
 	}
-
+	//1 hour
+	@Scheduled(fixedDelay=3600000)
+	public void reloadBlogs()
+	{
+		List<Blog> blogs = blogRepository.findAll();
+		for (Blog blog : blogs) {
+			saveItems(blog);
+		}
+	}
 }
